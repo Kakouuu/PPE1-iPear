3eaa46bb2a8ba7fc1c7801c43a6425cba69851d4
Ro6uby <101344066+Ro6uby@users.noreply.github.com>
2022-05-02 10:53:07 +0200

    test

--- nav.php
+++ nav.php
@@ -5,8 +5,13 @@
     <link rel="stylesheet" href="styles.css">
 
     <script src="https://kit.fontawesome.com/db2bf29261.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 </head>
+<?php
+
+require_once("db.php");
+?>
 
 <nav class="navbar" style="height: 50px !important;">
 
@@ -25,7 +30,7 @@
                 <li><a href="pagelegume.php?id=Fruits Rouge" title="" class="type_sous">Baies et fruits rouges</a></li>
                 <li><a href="pagelegume.php?id=Agrume" title="" class="type_sous">Agrumes</a></li>
                 <li><a href="pagelegume.php?id=Fruit à Coque" title="" class="type_sous">Fruits à coque</a></li>
-                <li><a href="pagelegume.php?id=Fruit Exotique" title="" class="type_sous">Fruits exotiques</a></li>
+                <li><a href="pagelegume.php?id=Exotique" title="" class="type_sous">Fruits exotiques</a></li>
             </ul>
         <li class="services"><a href="#" title="" class="type">Légumes</a>
             <ul class="sous-menu">
@@ -42,22 +47,27 @@
     </ul>
 
 
-
     <span class="cotedroit">
       <span class="search">
-            <input autocomplete="off" type="search" onkeyup="manakey(event)" name="Rechercher"
+            <input autocomplete="off" type="search" onkeyup="manakey(event)" oninput="salut()" name="Rechercher"
                    class="searchbar_container" placeholder="Rechercher">
             <span style="cursor: pointer;" onclick='sendad()'><i class="fa-solid fa-magnifying-glass"></i></span>
         </span>
+
+
+<div class="search_prod"></div>
+
+
+
+
+
+
 
             <a href="/" class="item"><i class="fa-solid fa-basket-shopping"></i></a>
 
             <a href="login.php" class="log">Connexion <i class="fa-solid fa-arrow-right-to-bracket"></i> </a>
 
     </span>
-
-
-
 
 
     <script>
@@ -71,10 +81,41 @@
             if (e.keyCode == 13) {
                 sendad();
             }
+            var clientHeight = document.querySelector('.search_prod').clientHeight;
+            console.log(clientHeight);
+
+            if (clientHeight < 356) {
+                document.querySelector(".plus").style.display = "none";
+            }
         }
 
-        
+        function salut() {
+            let display = document.querySelector('.search_prod').style.display = 'block';
+            var mot = document.getElementsByName('Rechercher')[0].value;
+            console.log(mot)
+
+            if(mot != "" ){
+                $.post("recherche.php", {mot : mot}, function(data){
+                    $('.search_prod').html(data);
+                });
+            }else
+                document.querySelector(".search_prod").style.display ="none";
+        }
+
+
+        function remouve(e) {
+            if (e.keyCode == 8) {
+                let display = document.querySelector('.search_prod').style.display = 'none';
+            }
+        }
+
+
+console.log(clientHeight);
+
+
     </script>
+
+
 
     <div class="navbar__toggle" onclick="openOrClose()" id="mobile-menu">
         <div class="tirait">
@@ -87,23 +128,23 @@
 <ul class="navbar_container2">
     <li class="services2"><a href="#" title="" class="type2">Fruits</a>
         <ul class="sous-menu2">
-            <li><a href="#" title="" class="type_sous2">Fruits à noyaux</a></li>
-            <li><a href="#" title="" class="type_sous2">Fruits à pépins</a></li>
-            <li><a href="#" title="" class="type_sous2">Baies et fruits rouges</a></li>
-            <li><a href="#" title="" class="type_sous2">Agrumes</a></li>
-            <li><a href="#" title="" class="type_sous2">Fruits à coque</a></li>
-            <li><a href="#" title="" class="type_sous2">Fruits exotiques</a></li>
+            <li><a href="pagelegume.php?id=Fruits Noyaux" title="" class="type_sous2">Fruits à noyaux</a></li>
+            <li><a href="pagelegume.php?id=Fruits Pépin" title="" class="type_sous2">Fruits à pépins</a></li>
+            <li><a href="pagelegume.php?id=Fruits Rouge" title="" class="type_sous2">Baies et fruits rouges</a></li>
+            <li><a href="pagelegume.php?id=Agrume" title="" class="type_sous2">Agrumes</a></li>
+            <li><a href="pagelegume.php?id=Fruit à Coque" title="" class="type_sous2">Fruits à coque</a></li>
+            <li><a href="pagelegume.php?id=Exotique" title="" class="type_sous2">Fruits exotiques</a></li>
         </ul>
     <li class="services2"><a href="#" title="" class="type2">Légumes</a>
         <ul class="sous-menu2">
-            <li><a href="#" title="" class="type_sous2">Légumes fleurs</a></li>
-            <li><a href="#" title="" class="type_sous2">Graines</a></li>
-            <li><a href="#" title="" class="type_sous2">Légumes feuilles</a></li>
-            <li><a href="#" title="" class="type_sous2">Légumes fruits</a></li>
-            <li><a href="#" title="" class="type_sous2">Racine</a></li>
-            <li><a href="#" title="" class="type_sous2">Légumes à bulbe</a></li>
-            <li><a href="#" title="" class="type_sous2">Légumes tubercules</a></li>
-            <li><a href="#" title="" class="type_sous2">Légumes tiges</a></li>
+            <li><a href="pagelegume.php?id=Légume Fleurs" title="" class="type_sous2">Légumes fleurs</a></li>
+            <li><a href="pagelegume.php?id=Légume Graines" title="" class="type_sous2">Graines</a></li>
+            <li><a href="pagelegume.php?id=Légume Feuilles" title="" class="type_sous2">Légumes feuilles</a></li>
+            <li><a href="pagelegume.php?id=Légume Fruit" title="" class="type_sous2">Légumes fruits</a></li>
+            <li><a href="pagelegume.php?id=Légume Racine" title="" class="type_sous2">Racine</a></li>
+            <li><a href="pagelegume.php?id=Légume à Bulbe" title="" class="type_sous2">Légumes à bulbe</a></li>
+            <li><a href="pagelegume.php?id=Légume Tubercules" title="" class="type_sous2">Légumes tubercules</a></li>
+            <li><a href="pagelegume.php?id=Légume Tige" title="" class="type_sous2">Légumes tiges</a></li>
         </ul>
 
 </ul>
