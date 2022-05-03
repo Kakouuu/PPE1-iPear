8bf80c23106a78eaa71d0b91853dba655b6ea24c
Ro6uby <101344066+Ro6uby@users.noreply.github.com>
2022-05-03 12:06:20 +0200

    Dossier Test/panier

--- test.php
+++ test.php
@@ -1,100 +1,66 @@
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="test.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="test.css">
+    <title>Panier</title>
+</head>
+<body>
 
-<div class="container padding-bottom-3x mb-1">
-    <!-- Shopping Cart-->
-    <div class="table-responsive shopping-cart">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Product Name</th>
-                <th class="text-center">Quantity</th>
-                <th class="text-center">Subtotal</th>
-                <th class="text-center">Discount</th>
-                <th class="text-center"><a class="btn btn-sm btn-outline-danger" href="#">Clear Cart</a></th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>
-                    <div class="product-item">
-                        <a class="product-thumb" href="#"><img src="https://via.placeholder.com/220x180/FF0000/000000" alt="Product"></a>
-                        <div class="product-info">
-                            <h4 class="product-title"><a href="#">Unionbay Park</a></h4><span><em>Size:</em> 10.5</span><span><em>Color:</em> Dark Blue</span>
-                        </div>
-                    </div>
-                </td>
-                <td class="text-center">
-                    <div class="count-input">
-                        <select class="form-control">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                </td>
-                <td class="text-center text-lg text-medium">$43.90</td>
-                <td class="text-center text-lg text-medium">$18.00</td>
-                <td class="text-center"><a class="remove-from-cart" href="#" data-toggle="tooltip" title="" data-original-title="Remove item"><i class="fa fa-trash"></i></a></td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="product-item">
-                        <a class="product-thumb" href="#"><img src="https://via.placeholder.com/220x180/5F9EA0/000000" alt="Product"></a>
-                        <div class="product-info">
-                            <h4 class="product-title"><a href="#">Daily Fabric Cap</a></h4><span><em>Size:</em> XL</span><span><em>Color:</em> Black</span>
-                        </div>
-                    </div>
-                </td>
-                <td class="text-center">
-                    <div class="count-input">
-                        <select class="form-control">
-                            <option>1</option>
-                            <option selected="">2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                </td>
-                <td class="text-center text-lg text-medium">$24.89</td>
-                <td class="text-center">—</td>
-                <td class="text-center"><a class="remove-from-cart" href="#" data-toggle="tooltip" title="" data-original-title="Remove item"><i class="fa fa-trash"></i></a></td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="product-item">
-                        <a class="product-thumb" href="#"><img src="https://via.placeholder.com/220x180/9932CC/000000" alt="Product"></a>
-                        <div class="product-info">
-                            <h4 class="product-title"><a href="#">Cole Haan Crossbody</a></h4><span><em>Size:</em> -</span><span><em>Color:</em> Turquoise</span>
-                        </div>
-                    </div>
-                </td>
-                <td class="text-center">
-                    <div class="count-input">
-                        <select class="form-control">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                </td>
-                <td class="text-center text-lg text-medium">$200.00</td>
-                <td class="text-center">—</td>
-                <td class="text-center"><a class="remove-from-cart" href="#" data-toggle="tooltip" title="" data-original-title="Remove item"><i class="fa fa-trash"></i></a></td>
-            </tr>
-            </tbody>
-        </table>
+<?php
+include('nav.php')
+?>
+<br> <br><br>
+<div style="margin-left : 100px; margin-right : 100px;">
+
+<h1 style="border-bottom : 1px solid black; padding-bottom:8px;">Panier</h1>
+<br>
+
+    <div class="all">
+        <div class="header">
+
+            <div class="nom">Nom du produit</div>
+
+            <div class="header_sous">   
+                <div>Quantité</div>
+                <div>Prix unitaire</div>
+                <div>Total</div>
+                <div>Supprimer le pannier</div>
+            </div>
+        </div>
+<hr>
+
+        <div class="produits">
+            <div style="display : flex;align-items : center;">
+            <div class="imgprod"></div> 
+            Pomme de terre charlotte</div>
+
+            <div class="header_sous">
+                <div><input id="number" type="number" value="1" min="1"></div>
+                <div class="PU">12€</div>
+                <div class="toto">12€</div>
+                <div class="trash">Poubelle</div>
+                 
+            </div>
+
+        </div>
+
+        
     </div>
-    <div class="shopping-cart-footer">
-        <div class="column text-lg">Subtotal: <span class="text-medium">$289.68</span></div>
-    </div>
-    <div class="shopping-cart-footer">
-        <div class="column"><a class="btn btn-outline-secondary" href="#"><i class="icon-arrow-left"></i>&nbsp;Back to Shopping</a></div>
-        <div class="column"><a class="btn btn-primary" href="#" data-toast="" data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Your cart" data-toast-message="is updated successfully!">Update Cart</a><a class="btn btn-success" href="#">Checkout</a></div>
-    </div>
+
+        <div class="total">
+            Prix total : 200,50 €
+        </div>
+<br>
+        <div class="bas">
+            <a href="index.php"><div class="retour">Retour à l'accueil</div></a>
+                <div class="commande">Paiement</div>
+        </div>
+
 </div>
+
+
+</body>
+</html>
