1da1d1b5799148b31ec658137e6f70aacc84ecc0
Kakouuu <cyrikchenpro@gmail.com>
2022-05-16 12:28:15 +0200

    Patch Update quantite addpanier

--- addpanier.php
+++ addpanier.php
@@ -6,11 +6,23 @@ if(!isset($_SESSION['ID'])){
 }
 
 
-if(isset($_GET['idprod'],$_GET['qteprod'])&& isset($_SESSION["ID"])){
-    $sql = "INSERT INTO panier (ID_user,ID_FL,quantite) VALUES ('".$_SESSION['ID']."','".$_GET['idprod']."','".$_GET['qteprod']."')";
+//if(isset($_GET['idprod'],$_GET['qteprod'])){
+//    $sql = "INSERT INTO panier (ID_user,ID_FL,quantite) VALUES ('".$_SESSION['ID']."','".$_GET['idprod']."','".$_GET['qteprod']."')";
+//    $result = mysqli_query($mysqli, $sql);
+//    $_SESSION['valid'] = 1;
+//    header("Location:productdetail.php?id=".$_GET["idprod"]);
+//}
+
+$sql = @mysqli_query("SELECT * FROM panier WHERE ID_user = '".$_SESSION['ID']."' and ID_FL = '".$_GET['idprod']."'");
+if(mysqli_num_rows($sql) < 1) {
+
+    $sql = "INSERT INTO panier (ID_user,ID_FL,quantite) 
+            VALUES ('" . $_SESSION['ID'] . "','" . $_GET['idprod'] . "','" . $_GET['qteprod'] . "')
+            ON DUPLICATE KEY UPDATE quantite = quantite + '" . $_GET['qteprod'] . "'";
+
     $result = mysqli_query($mysqli, $sql);
     $_SESSION['valid'] = 1;
-    header("Location:productdetail.php?id=".$_GET["idprod"]);
-}
+    header("Location:productdetail.php?id=" . $_GET["idprod"]);
 
+}
 ?>
