a37304864dac23b1dce0356b04ba5ae8da9e586f
Kakouuu <cyrikchenpro@gmail.com>
2022-05-19 17:23:52 +0200

    Working AJAX

--- update.php
+++ update.php
@@ -1,12 +1,10 @@
 <?php
 include "db.php";
-
 if(isset($_POST['idproduct'],$_POST['qteprod'])) {
 
     $idprodct = $_POST['idproduct'];
     $qte = $_POST['qteprod'];
-
-    $sql = "UPDATE `panier` SET quantite = '$qte' WHERE ID_user = '" . $_SESSION['ID_user'] . "' and ID_FL ='$idprodct'";
+    $sql = "UPDATE `panier` SET quantite = '$qte' WHERE ID_user = '" . $_SESSION['ID'] . "' and ID_FL ='$idprodct'";
     mysqli_query($mysqli, $sql);
 }
 ?>
