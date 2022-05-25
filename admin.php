585c5f5a5fd76e0bee9e21eefc7fc3629f3ad73a
Ro6uby <101344066+Ro6uby@users.noreply.github.com>
2022-05-25 11:59:07 +0200

    nfzeorfn

--- admin.php
+++ admin.php
@@ -31,6 +31,7 @@ if ($_SESSION['ID'] != 6) {?>
     <a href="/" class="prout"><div>Ajouter un produit</div></a>
     <a href="/" class="prout"><div>Modifier un produit</div></a>
     <a href="gerer.php" class="prout"><div>Gérer les utilisateurs</div></a>
+    mkz,fmze,
 
     
 </div>
--- traitementnewuser.php
+++ traitementnewuser.php
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-<?php
-require_once "db.php";
-session_start();
-?>
-
-<?php
-$Prenom = addslashes($_POST["prenom"]);
-$Nom = addslashes($_POST["nom"]);
-$Mail = addslashes($_POST["mail"]);
-$NumTel = addslashes($_POST["tel"]);
-$Adresse = addslashes($_POST["poste"]);
-$Mdp = addslashes($_POST["mdp"]);
-$hashed_password = password_hash($Mdp, PASSWORD_DEFAULT);
-
-$sql = "insert into utilisateurs (Nom, Prenom, Mail, NumTel, Mdp, Adresse) " .
-    " values('" . $Nom . "', '" .  $Prenom . "','" .  $Mail  . "','" .  $NumTel . "','" . $hashed_password . "','" . $Adresse . "')";
-
-mysqli_query($mysqli, $sql);
-
-header("location: index.php");
-?>
-=======
 <?php
 require_once "db.php";
 include_once ('phpmailer.php');
@@ -104,4 +81,3 @@ if (isset($_POST['submit'])) {
 
 </body>
 </html>
->>>>>>> 0a5e24099b8a7b41277550a6047962ea261af199
